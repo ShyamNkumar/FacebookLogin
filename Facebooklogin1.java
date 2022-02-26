28c4b7ee29842517adc32b1401a6e7588ce9da60
ShyamNkumar <99385805+ShyamNkumar@users.noreply.github.com>
2022-02-26 08:58:27 +0530

    Name Add
    Adding the Names to Facebook login1 Brach

--- Facebooklogin1.java
+++ Facebooklogin1.java
@@ -2,7 +2,9 @@ public void Facebooklogin1
  {
 public static void main(String[] args)
 {
-
+System.out.println("Shyam");
+System.out.println("Sam");
+System.out.println("Sham");
 System.out.println("Coimbatore");
 System.out.println("Chennai");
 }
